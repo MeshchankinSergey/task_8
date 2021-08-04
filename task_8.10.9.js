6297f571139655927bd2cd5527761e3e5c4ca972
Meshchankin Sergey <meshchankin.s@ya.ru>
2021-08-04 19:21:36 +0500

    add file

---
+++ task_8.10.9.js
@@ -0,0 +1,9 @@
+function parityElements(arr = [0, 0, 0]) {
+
+console.log("количество четных чисел =", arr.filter(n => typeof(n) == "number" && n%2 == 0 && n/2 !== 0).length);
+
+console.log("количество нечетных чисел =", arr.filter(n => typeof(n) == "number" && n%2 !== 0).length);
+console.log("количество нулевых элементов =", arr.filter(n => typeof(n) == "number" && n/2 == 0).length);
+};
+
+parityElements();
